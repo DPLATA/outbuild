4c5d9dabe34e5fa32cd14cbb6fae42b366708c8f
Silverboy <diegogplata@gmail.com>
2024-10-15 21:36:36 -0600

    feat: initial commit db setup

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const config = {
+  username: process.env.DB_USER || '',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || '',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  dialect: 'postgres'
+};
+
+module.exports = config;
