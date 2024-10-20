720d4a1c29e9a238aee9fc3392916203990672a2
Silverboy <diegogplata@gmail.com>
2024-10-19 20:35:19 -0600

    fix: past version of config

--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,7 @@ const config = {
   username: process.env.DB_USER || '',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || '',
-  //host: process.env.DB_HOST || 'localhost',
-  host: process.env.CI ? 'db' : 'localhost',
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
   dialect: 'postgres'
 };
