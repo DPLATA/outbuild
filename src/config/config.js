058e99e27d80213875a27be41f52d007f866fc26
Silverboy <diegogplata@gmail.com>
2024-10-19 20:08:20 -0600

    fix: work already man!!

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,8 @@ const config = {
   username: process.env.DB_USER || '',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || '',
-  host: process.env.DB_HOST || 'localhost',
+  //host: process.env.DB_HOST || 'localhost',
+  host: '127.0.0.1',
   port: parseInt(process.env.DB_PORT || '5432', 10),
   dialect: 'postgres'
 };
